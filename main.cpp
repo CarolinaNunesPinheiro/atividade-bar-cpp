95ef1b668f67116ba926b67795713822e2ab5be3
Carolina Nunes Pinheiro <nunespcarolina@gmail.com>
2020-05-07 18:56:49 -0300

    Leitura dos dados

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+string sexo ;
+int cervejas, refri, espetos;
+
+cout << "Sexo: ";
+cin >> sexo;
+cout << "Quantidade de cervejas: ";
+cin >> cervejas;
+cout << "Quantidade de refrigerantes: ";
+cin >> refri;
+cout << "Quantidade de espetinhos: ";
+cin >> espetos;
+
+    return 0;
+}
